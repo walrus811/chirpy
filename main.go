32370a9fd36d01d6c36815f15b5b542562e93602
ray <walrusray811@gmail.com>
2024-05-21 14:13:05 +0900

    to ch3

--- main.go
+++ main.go
@@ -23,18 +23,28 @@ func main() {
 	cfg := &apiConfig{fileserverHits: 0}
 	mux := http.NewServeMux()
 
-	mux.Handle("/app/*", http.StripPrefix("/app/", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	mux.Handle("GET /app/*", http.StripPrefix("/app/", cfg.middlewareMetricsInc(http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Add("Content-Type", "text/plain;charset=UTF-8")
 		w.Write([]byte("OK"))
 	})
-	mux.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /api/metrics", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		w.Header().Add("Content-Type", "text/plain;charset=UTF-8")
 		w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits)))
 	})
-	mux.HandleFunc("/reset", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Header().Add("Content-Type", "text/html")
+		w.Write([]byte(fmt.Sprintf(`<html>
+		<body>
+				<h1>Welcome, Chirpy Admin</h1>
+				<p>Chirpy has been visited %d times!</p>
+		</body>
+		</html>`, cfg.fileserverHits)))
+	})
+	mux.HandleFunc("GET /api/reset", func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits = 0
 		w.WriteHeader(http.StatusOK)
 		w.Header().Add("Content-Type", "text/plain;charset=UTF-8")
